3954e881ed0cd5db658672a9a0b847dec8968ec9
Amer Almohammad <ajaber1973@web.de>
2025-09-26 10:51:55 +0200

    Update infrastructure.ts

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -6,10 +6,14 @@ const app = new cdk.App();
 
 new TaskMasterInfrastructureStack(app, "TaskMasterInfrastructureStack", {
   env: {
-    account: "171158265889",
+    account: "your_AWS_account",
     region: "eu-central-1",
   },
   description: "TaskMaster Pro - MultiSaaS Platform Infrastructure",
 });
 
 app.synth();
+
+
+
+
